015032e9d5190e1408cf7261b7e92dea7dc5ea4f
mahesh.ganji <mahesh050917@gmail.com>
2026-04-09 12:19:34 +0530

    terraform code updated

---
+++ terraform-mulitenv/tfvars/dev/backend.tf
@@ -0,0 +1,6 @@
+bucket = "dev-terraform-devops88s"
+region = "us-east-1"
+encrypt = true 
+key = "dev-terraform-devops88s.tfstate"
+use_lockfile = true
+            
---
+++ terraform-mulitenv/tfvars/prod/backend.tf
@@ -0,0 +1,7 @@
+ bucket = "prod-terraform-devops88s"
+ region = "us-east-1"
+ encrypt = true 
+ key = "prod-terraform-devops88s.tfstate"
+ use_lockfile = true
+     
+   
